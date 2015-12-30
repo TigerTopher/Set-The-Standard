8eea164c3e29da970b062bbee5f9298721094b5c
Crisostomo Ibarra <christov9999@gmail.com>
2015-12-30 23:51:52 +0800

    Game Added with BG + File Organized

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var bgImage = new Image();
 bgImage.onload = function () {
 	bgReady = true;
 };
-bgImage.src = "game-background.jpg";
+bgImage.src = "images/background.png";
 
 // Hero image
 var heroReady = false;
